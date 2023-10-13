21e570635e1b98fd2338dfe2a7c810e3d6dbdef0
Arthur Monney <monneylobe@gmail.com>
2023-10-13 07:09:20 +0200

    :heavy_plus_sign: Add React Query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from 'next/app'
+import QueryProvider from '@/providers/QueryProvider'
 import StoreProvider from '@/providers/StoreProvider'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <StoreProvider>
-      <Component {...pageProps} />
+      <QueryProvider>
+        <Component {...pageProps} />
+      </QueryProvider>
     </StoreProvider>
   )
 }
